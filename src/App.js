84730dfde3c10a2b8329d624fd498f364a42c2d4
payalkherajani <payal.kherajani19@gmail.com>
2020-12-28 21:44:29 +0530

    Added-Functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [birth,setBirth] = useState();
+  const [lucky,setLucky] = useState(0);
+
+  function inputHandler(event){
+    const userBirth = event.target.value;
+    setBirth(new Date(userBirth));
+
+  }
+  function getLuckyNum(event){
+    const luckyNumber = event.target.value;
+    setLucky(luckyNumber);
+
+  }
+
+  function evaluateFunc(){
+  const dateUser = birth;
+  const year = dateUser.getFullYear();
+  const month = dateUser.getMonth() + 1 ;
+  const date = dateUser.getDate();
+  const dividend = year + month + date;
+  
+  const result = dividend%lucky;
+  if(result==0){
+    alert("Yeah!Lucky")
+  }
+  else{
+    alert("Oops!Change your lucky number");
+  }
+  
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="date" onChange={inputHandler} />
+      <input type="number" placeholder="Input your lucky number" onChange={getLuckyNum} />
+      <button onClick={evaluateFunc}>Evaluate</button>
+      <div>Output</div> 
     </div>
   );
 }
